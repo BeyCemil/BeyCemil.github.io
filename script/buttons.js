f584a0671aaae349d61662d01397342f6d2ba6a5
Cemil Buğra Uzunkaya <uzunkayacemil18@gmail.com>
2023-11-28 18:04:40 +0300

    File Organization

---
+++ script/buttons.js
@@ -0,0 +1,7 @@
+document.getElementById("portfolio-button-js").addEventListener("click", function () {
+    window.location.href = "https://flowcv.com/resume/dt7clncdsc";
+});
+
+document.getElementById("contact-button-js").addEventListener("click", function () {
+    window.location.href = "https://www.linkedin.com/in/cemilbey/";
+});
